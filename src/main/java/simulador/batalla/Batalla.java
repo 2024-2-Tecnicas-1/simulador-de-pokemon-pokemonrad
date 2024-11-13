c1d2a5716ce85a8f3095aa375d47893808389be3
Juan Acosta <juanestebanacostarodriguez@gmail.com>
2024-11-13 12:58:17 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 8737507bd2be6696e88318d3091d24087f00eb7d, reversing
    changes made to 67161dd61bc6fb095819441641ecadd9dbfdbe2b.

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,39 +1,5 @@
 package simulador.batalla;
 
-import simulador.pokemon.Pokemon;
-import java.util.Random;
-
 public class Batalla {
-    public static void iniciarBatalla(Pokemon p1, Pokemon p2) {
-        System.out.println("¡Comienza la batalla entre " + p1.getNombre() + " y " + p2.getNombre() + "!");
-
-        Random random = new Random();
-        boolean turnoP1 = random.nextBoolean(); 
-
-        while (p1.getSalud() > 0 && p2.getSalud() > 0) {
-            if (turnoP1) {
-                System.out.println("\nTurno de " + p1.getNombre() + ":");
-                p1.habilidadEspecial();
-                p1.atacar(p2);
-                
-                if (p2.getSalud() <= 0) {
-                    System.out.println(p2.getNombre() + " ha sido derrotado.");
-                    System.out.println(p1.getNombre() + " es el ganador!");
-                    break;
-                }
-            } else {
-                System.out.println("\nTurno de " + p2.getNombre() + ":");
-                p2.habilidadEspecial();
-                p2.atacar(p1);
-
-                if (p1.getSalud() <= 0) {
-                    System.out.println(p1.getNombre() + " ha sido derrotado.");
-                    System.out.println(p2.getNombre() + " es el ganador!");
-                    break;
-                }
-            }
-
-            turnoP1 = !turnoP1;
-        }
-    }
+    // TODO: Aquí va tu código
 }
